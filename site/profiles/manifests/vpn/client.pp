1428f8ee9f4052c60755e6717417b649f12cf6a1
maxkerrer <maxkerrer@126.com>
2017-04-23 13:25:57 +0800

    change hira data2

--- site/profiles/manifests/vpn/client.pp
+++ site/profiles/manifests/vpn/client.pp
@@ -4,47 +4,9 @@
 class profiles::vpn::client (
     $remote
 ) {
-    package { 'openvpn':
-        ensure  => present,
-        require => [
-            Service['dnsmasq'],
-        ],
-    } ->
-    file { '/etc/openvpn/keys':
-        ensure => directory,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0400',
-    } ->
-    file { "/etc/openvpn/${::fqdn}.conf":
-        ensure  => present,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0600',
-        content => template('profiles/vpn/client.conf.erb'),
-    } ~>
-    service { 'openvpn':
-        ensure    => running,
-        name      => "openvpn@${::fqdn}",
-        hasstatus => true,
-        enable    => true,
-        subscribe => [
-            Service['dnsmasq'],
-        ],
-    }
-
-    # Accept all via vpn
-    firewall { '200 accept input via VPN':
-        chain   => 'INPUT',
-        iniface => 'tun+',
-        action  => 'accept',
-        proto   => 'all',
-    }
-
-    firewall { '201 accept input via VPN':
-        chain   => 'FORWARD',
-        iniface => 'tun+',
-        action  => 'accept',
-        proto   => 'all',
-    }
+   class { 'hosts':
+    one_primary_ipv4 => false, 
+    one_primary_ipv6 => false,
+    entries => { '192.168.2.1' => [ 'foo.example.org', 'foo' ] }
+   }
 }
